2562d4f02ab1f7166bcae5b8bc713c4afd367bb4
Araz Sultanian <araz.sultanian@perficient.com>
2023-07-18 18:46:02 -0700

    test

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <Header />
+    <router-view />
+    <Footer />
   </div>
 </template>
+
+<script>
+import Header from '~/components/Header.vue';
+import Footer from '~/components/Footer.vue';
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to the Barbershop</h1>
+    <Services />
+  </div>
+</template>
+
+<script>
+import Services from '~/components/Services.vue';
+
+export default {
+  components: {
+    Services
+  }
+};
+</script>
