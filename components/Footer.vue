6a36fe145c54edb26e5129e4d14815e22dc9970f
Araz Sultanian <araz.sultanian@gmail.com>
2023-07-18 19:26:29 -0700

    testing

---
+++ components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <h1>Barbershop Logo</h1>
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/services">Services</router-link></li>
+        <li><router-link to="/contact">Contact</router-link></li>
+      </ul>
+    </nav>
+  </header>
+</template>
